3a12b8ceb3f999581bbf84df4e9cc80586e16b78
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2024-12-22 22:06:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = "us-east-1" # Update this with your region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0583d8c7a9c35822c" # Replace with your AMI ID
+resource "aws_instance" "AAP" {
+  ami           = "ami-01816d07b1128cd2d" # Replace with your AMI ID
   instance_type = var.instance_type       # Use variable for instance type
   key_name      = "myfirst"
   tags = {
-    Name = "DailyInstanceTypeChange"
+    Name = "test_upgrade_new"
   }
 }
-
 
 variable "instance_type" {
   description = "Instance type to set for the EC2 instance"
   type        = string
   default     = "t2.micro" # Default target instance type
 }
+
