43024bd994ae46c9802ee705b43ea1abac96bf9c
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2024-12-22 12:44:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your AMI ID
+  ami           = "ami-0583d8c7a9c35822c" # Replace with your AMI ID
   instance_type = var.instance_type       # Use variable for instance type
   tags = {
     Name = "DailyInstanceTypeChange"
